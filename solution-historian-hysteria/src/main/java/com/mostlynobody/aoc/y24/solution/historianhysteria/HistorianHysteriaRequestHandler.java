548f7dc1560a9e64c5d7e2028b1930ca43c5a50d
mostlynobody <32034333+mostlynobody@users.noreply.github.com>
2024-12-02 10:39:49 +0100

    improved lambda function: solution-historian-hysteria

--- solution-historian-hysteria/src/main/java/com/mostlynobody/aoc/y24/solution/historianhysteria/HistorianHysteriaRequestHandler.java
+++ solution-historian-hysteria/src/main/java/com/mostlynobody/aoc/y24/solution/historianhysteria/HistorianHysteriaRequestHandler.java
@@ -12,6 +12,8 @@ import jakarta.inject.Inject;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 import static java.lang.Math.abs;
@@ -38,23 +40,27 @@ public class HistorianHysteriaRequestHandler extends MicronautRequestHandler<API
         }
 
         ArrayList<Integer> leftList = new ArrayList<>(), rightList = new ArrayList<>();
+        HashMap<Integer, Integer> leftMap = new HashMap<>(), rightMap = new HashMap<>();
+
         Utils.removeLastLineBreak(inputJson.value()).lines().forEach(line -> {
-            var nums = line.split(" {3}");
-            leftList.add(Integer.parseInt(nums[0]));
-            rightList.add(Integer.parseInt(nums[1]));
+            var nums = Arrays.stream(line.split(" \\s+")).map(Integer::parseInt).toList();
+            leftList.add(nums.get(0));
+            rightList.add(nums.get(1));
+            leftMap.put(nums.get(0), 1 + leftMap.getOrDefault(nums.get(0), 0));
+            rightMap.put(nums.get(1), 1 + rightMap.getOrDefault(nums.get(1), 0));
         });
 
         leftList.sort(Integer::compareTo);
         rightList.sort(Integer::compareTo);
 
         int silver = 0;
-        long gold = 0;
         for (int i = 0; i < leftList.size(); i++) {
-            int l = leftList.get(i);
-
-            silver += abs(l - rightList.get(i));
-            gold += l * rightList.stream().filter(r -> r == l).count();
+            silver += abs(leftList.get(i) - rightList.get(i));
         }
+
+        long gold = leftMap.entrySet().stream()
+                .map(e -> (long) e.getValue() * e.getKey() * rightMap.getOrDefault(e.getKey(), 0))
+                .reduce(0L, Long::sum);
 
         try {
             String responseBody = objectMapper.writeValueAsString(new SolutionJson(String.valueOf(silver), String.valueOf(gold)));
