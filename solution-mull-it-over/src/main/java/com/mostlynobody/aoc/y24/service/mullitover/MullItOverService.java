ef7de57abb219902dd1471acf81e0c1dba75d79a
Tycho <32034333+mostlynobody@users.noreply.github.com>
2024-12-03 15:24:25 +0000

    updated service MullItOverService

--- solution-mull-it-over/src/main/java/com/mostlynobody/aoc/y24/service/mullitover/MullItOverService.java
+++ solution-mull-it-over/src/main/java/com/mostlynobody/aoc/y24/service/mullitover/MullItOverService.java
@@ -8,21 +8,21 @@ import java.util.regex.Pattern;
 
 public class MullItOverService implements SolutionService {
 
+    private static final Pattern MUL_PATTERN = Pattern.compile("mul\\((\\d{1,3}),(\\d{1,3})\\)");
+    private static final Pattern DO_PATTERN = Pattern.compile("(?:^|do\\(\\)).*?(?:don't\\(\\)|$)", Pattern.DOTALL);
+
     @Override
     public SolutionJson solve(String rawInput) {
-        int silver = getMulSum(rawInput);
-        int gold = 0;
+        int silver = calculateMulSum(rawInput);
 
-        Matcher doMatcher = Pattern.compile("(?:^|do\\(\\)).*?(?:don't\\(\\)|$)", Pattern.DOTALL).matcher(rawInput);
-        while (doMatcher.find()) gold += getMulSum(doMatcher.group(0));
+        Matcher doMatcher = DO_PATTERN.matcher(rawInput);
+        int gold = doMatcher.results().mapToInt(it -> calculateMulSum(it.group(0))).sum();
 
         return new SolutionJson(String.valueOf(silver), String.valueOf(gold));
     }
 
-    private int getMulSum(String input) {
-        int sum = 0;
-        Matcher mulMatcher = Pattern.compile("mul\\((\\d{1,3}),(\\d{1,3})\\)").matcher(input);
-        while (mulMatcher.find()) sum += Integer.parseInt(mulMatcher.group(1)) * Integer.parseInt(mulMatcher.group(2));
-        return sum;
+    private int calculateMulSum(String input) {
+        Matcher mulMatcher = MUL_PATTERN.matcher(input);
+        return mulMatcher.results().mapToInt(it -> Integer.parseInt(it.group(1)) * Integer.parseInt(it.group(2))).sum();
     }
 }
